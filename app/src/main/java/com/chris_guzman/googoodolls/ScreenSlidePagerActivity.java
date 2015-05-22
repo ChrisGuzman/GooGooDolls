965975b635fd4f490b8afc08541f783b106c92ef
Chris Guzman <cguzman@orderup.com>
2015-05-22 13:10:21 -0400

    Switch pictures and text on swipe

--- app/src/main/java/com/chris_guzman/googoodolls/ScreenSlidePagerActivity.java
+++ app/src/main/java/com/chris_guzman/googoodolls/ScreenSlidePagerActivity.java
@@ -69,26 +69,23 @@ public class ScreenSlidePagerActivity extends FragmentActivity {
 
             Bundle bundle = new Bundle();
             switch (position) {
-                case 1:
+                case 0:
                     bundle.putString("TITLE", "Group Ordering");
                     bundle.putString("MSG", "Four dollar toast gluten-free slow-carb deep v. Tilde sustainable meh hoodie, chambray biodiesel lo-fi mixtape Austin cred Tumblr");
                     bundle.putInt("PIC", R.drawable.tacos);
                     break;
-                case 2:
+                case 1:
                     bundle.putString("TITLE", "Order Ahead");
                     bundle.putString("MSG", "Meowwww. Hiss at vacuum cleaner stare at the wall, play with food and get confused by dust yet hide at bottom of staircase to trip human knock dis off table");
                     bundle.putInt("PIC", R.drawable.meal);
                     break;
-                case 3:
+                case 2:
                     bundle.putString("TITLE", "Synergy");
                     bundle.putString("MSG", "Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits");
                     bundle.putInt("PIC", R.drawable.eggs);
                     break;
 
             }
-            bundle.putString("TITLE", "Group Ordering");
-            bundle.putString("MSG", "Four dollar toast gluten-free slow-carb deep v. Tilde sustainable meh hoodie, chambray biodiesel lo-fi mixtape Austin cred Tumblr");
-            bundle.putInt("PIC", R.drawable.tacos);
             frag.setArguments(bundle);
 
             return frag;
